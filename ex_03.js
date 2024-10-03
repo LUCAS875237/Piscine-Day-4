dfdcf789932b9c39975abbd38f57ed156f1c8c13
LUCAS875237 <lucas.renault@epitech.digital>
2024-10-03 16:47:15 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,7 @@
+window. onload = function(){
+    const listen = document. createElement ("p");
+    listen. textContent = "Now featuring a headphone jack!"
+    const desc = document.getElementById("description");
+    desc.appendChild(listen)
+
+}
